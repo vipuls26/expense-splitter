57b994b2c34638aac0f5fe04b3a92afb88273fff
vipuls26 <vipuls@patoliyainfotech.com>
2026-06-29 18:27:49 +0530

    make phone_no column unique in users table

--- backend/database/migrations/2026_06_23_110420_add_phone_number_to_users_table.php
+++ backend/database/migrations/2026_06_23_110420_add_phone_number_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone_no', 20)->nullable()->default(null);
+            $table->string('phone_no', 20)->nullable()->unique();
         });
     }
 
